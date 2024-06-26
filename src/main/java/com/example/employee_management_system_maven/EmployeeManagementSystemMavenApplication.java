5b9a7e7336ff570d9d8ce851c16a61ead2a5eefc
subha <subha@SUBHASHIS>
2024-06-26 09:37:35 +0530

    Initial commit

---
+++ src/main/java/com/example/employee_management_system_maven/EmployeeManagementSystemMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_management_system_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemMavenApplication.class, args);
+	}
+
+}
